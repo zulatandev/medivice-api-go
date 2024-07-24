19a2136c5c4e0dc578cd2c8f06f7dbb507be039c
zulatandev <supercleaver112@gmail.com>
2024-07-24 08:18:51 +0200

    add put and delete endpoints

--- api/todo.go
+++ api/todo.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -18,19 +20,80 @@ var todos []Todo
 func Handler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		json.NewEncoder(w).Encode(todos)
+		handleGetTodos(w, r)
 	case "POST":
-		var todo Todo
-		err := json.NewDecoder(r.Body).Decode(&todo)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		todo.ID = len(todos) + 1
-		todo.CreateDate = time.Now()
-		todos = append(todos, todo)
-		json.NewEncoder(w).Encode(todo)
+		handleCreateTodo(w, r)
+	case "PUT":
+		handleUpdateTodo(w, r)
+	case "DELETE":
+		handleDeleteTodo(w, r)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
+}
+
+func handleGetTodos(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todos)
+}
+
+func handleCreateTodo(w http.ResponseWriter, r *http.Request) {
+	var todo Todo
+	err := json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	todo.ID = len(todos) + 1
+	todo.CreateDate = time.Now()
+	todos = append(todos, todo)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todos)
+}
+
+func handleUpdateTodo(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/todos/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	var updatedTodo Todo
+	err = json.NewDecoder(r.Body).Decode(&updatedTodo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for i, todo := range todos {
+		if todo.ID == id {
+			todos[i].Title = updatedTodo.Title
+			todos[i].Completed = updatedTodo.Completed
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(todos[i])
+			return
+		}
+	}
+
+	http.Error(w, "Todo not found", http.StatusNotFound)
+}
+
+func handleDeleteTodo(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/todos/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	for i, todo := range todos {
+		if todo.ID == id {
+			todos = append(todos[:i], todos[i+1:]...)
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	}
+
+	http.Error(w, "Todo not found", http.StatusNotFound)
 }
